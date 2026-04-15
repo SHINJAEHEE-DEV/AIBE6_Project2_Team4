b00f83f403329a641e4ce5010c2e384cff2bf296
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-04-16 01:50:33 +0900

    커멘드 입력 구현중

--- src/main/java/org/project/App.java
+++ src/main/java/org/project/App.java
@@ -1,5 +1,7 @@
 package org.project;
 
+import java.util.Scanner;
+
 public class App {
 
     void run(){
@@ -8,6 +10,24 @@ public class App {
                 =========== CommandMonster ===========
                 
                 """);
+
+        while (true){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Command : ");
+            String cmd = scanner.nextLine();
+            switch (cmd){
+                case "시작" -> System.out.println("게임을 시작합니다1");
+                case "종료" -> {
+                    return;
+                }
+                default -> System.out.println("<알림!>제시된 올바른 커멘드를 입력하세요.");
+            }
+
+        }
+
+
     }
+
+
 
 }
