50f8199c4c9ffc735582f135e3a45270dfd98c4e
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-04-16 01:08:56 +0900

    파일 구조 생성

--- src/main/java/org/project/App.java
+++ src/main/java/org/project/App.java
@@ -3,7 +3,11 @@ package org.project;
 public class App {
 
     void run(){
-        System.out.println("==== 자바 CLI 서비스 만들기 ====");
+        System.out.println("""
+                
+                =========== CommandMonster ===========
+                
+                """);
     }
 
 }
---
+++ src/main/java/org/project/Repository/MonsterRepository.java
@@ -0,0 +1,4 @@
+package org.project.Repository;
+
+public class MonsterRepository {
+}
