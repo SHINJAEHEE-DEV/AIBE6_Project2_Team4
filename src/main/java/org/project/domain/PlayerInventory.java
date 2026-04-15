ec2e0bc957ecfcd40972b7c747587e32ad5e6a51
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-04-16 02:43:42 +0900

    플레이어 인벤토리 객체 제작, 보유 포켓몬 목록 및 포켓몬 추가 함수 제작

--- src/main/java/org/project/domain/PlayerInventory.java
+++ src/main/java/org/project/domain/PlayerInventory.java
@@ -1,4 +1,42 @@
 package org.project.domain;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 public class PlayerInventory {
+    private List<CommandMonster> myMonsters;
+    private final int MAX_CAPACITY = 6;
+
+    public PlayerInventory() {
+        this.myMonsters = new ArrayList<>();
+    }
+
+    // 포켓몬 추가 (6마리 제한 체크)
+    public boolean addMonster(CommandMonster monster) {
+        if (myMonsters.size() >= MAX_CAPACITY) {
+            System.out.println("\n[경고] 인벤토리가 가득 찼습니다! (최대 6마리)");
+            return false;
+        }
+        myMonsters.add(monster);
+        return true;
+    }
+
+    // 현재 보유한 포켓몬 목록 출력
+    public void showMonsters() {
+        System.out.println("\n========= [ 내 포켓몬 목록 ] =========");
+        if (myMonsters.isEmpty()) {
+            System.out.println("보유한 포켓몬이 없습니다.");
+        } else {
+            for (int i = 0; i < myMonsters.size(); i++) {
+                CommandMonster m = myMonsters.get(i);
+                System.out.printf("%d. %s (Lv.%d) [HP: %d/%d]\n",
+                        (i + 1), m.getName(), m.getLevel(), m.getCurrentHp(), m.getMaxHp());
+            }
+        }
+        System.out.println("======================================");
+    }
+
 }
